1f5923c2a20e4dcde43b94bddbe06bef234f05a4
sudharshan414 <99274967+sudharshan414@users.noreply.github.com>
2022-03-04 17:21:32 +0530

    Create disp.java

---
+++ display/disp.java
@@ -0,0 +1,13 @@
+package javaexamples;
+
+public class Age_Details {
+public static void main(String args[]) {
+	int age=10;
+	if(age>=10) {
+		System.out.println("age is 10");
+		}
+	else {
+		System.out.println("not age 10");
+	}
+	}
+}
